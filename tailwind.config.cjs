382885359a906f59ef9c0419b7ef7b705331ba76
Mark <103870627+Marrek1978@users.noreply.github.com>
2023-01-18 17:31:19 -0700

    UC for large with complete

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+        'hero': "url('./src/images/bg.png')"
+				// 'hero-pattern': "url('/img/hero-pattern.svg')",
+      }
+		}
+	},
+	plugins: [],
+}
